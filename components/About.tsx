2f3cb0704c998b902570c9326a7f57809e282367
SHIGIVAHAN ATHITHAN <141015788+ShigivahanA@users.noreply.github.com>
2025-06-19 17:14:35 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -21,7 +21,7 @@ export function FadeInParagraph() {
   return (
     <motion.p
       ref={ref}
-      className="text-xl lg:text-2xl !leading-relaxed mx-auto max-w-5xl text-center"
+      className="text-base sm:text-lg md:text-xl lg:text-2xl leading-relaxed sm:leading-loose mx-auto max-w-[90%] sm:max-w-3xl text-center px-4"
       initial="hidden"
       animate={controls}
       variants={{
